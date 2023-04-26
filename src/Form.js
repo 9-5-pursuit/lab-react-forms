7ab3b484ccf470a6442232bbc499f23db41e8286
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-04-25 22:46:25 -0400

    Got the math done

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [values, setValues] = useState("");
+  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState("");
+
+  function handleVal(values) {
+    // let val = values;
+    // let input = val.target.value;
+    // setValues(inputArray);
+    // console.log(values);
+  }
+
+  function handleOperation() {}
+
+  function handleSum(array) {
+    return array.reduce((a, b) => a + b, 0);
+  }
+
+  function handleAvg(array) {
+    return handleSum(array) / array.length;
+  }
+
+  function handleMode(array) {
+    return array
+      .sort(
+        (a, b) =>
+          array.filter((ele) => ele === a).length -
+          array.filter((ele) => ele === b).length
+      )
+      .pop();
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let inputArray = values.split(",");
+    let parseArray = inputArray.map((input) => parseInt(input));
+    if (parseArray.value === "number") {
+      if (operation === "sum") {
+        setResult(handleSum(parseArray));
+      } else if (operation === "average") {
+        setResult(handleAvg(parseArray));
+      } else if (operation === "mode") {
+        setResult(handleMode(parseArray));
+      }
+    } else {
+      setResult("Invalid input");
+    }
+  }
+  //
+
+  function handleResult() {}
+
   return (
     <>
       <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+        <input
+          id="values"
+          name="values"
+          type="text"
+          value={values}
+          onChange={(e) => setValues(e.target.value)}
+        />
+        <select
+          id="operation"
+          name="operation"
+          value={operation}
+          onChange={(e) => setOperation(e.target.value)}
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
           <option value="mode">mode</option>
         </select>
-        <button type="submit">Calculate</button>
+        <button type="submit" onClick={handleSubmit}>
+          Calculate
+        </button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </>
   );
