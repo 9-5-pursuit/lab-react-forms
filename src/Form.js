9148896e648f29e34423cb646e71a1475154b9f5
Jameel Ibrahim <jameelibrahim@Jameels-MacBook-Air.local>
2023-04-26 09:34:05 -0400

    Lab

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [values, setValues] = useState("");
+  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState("");
+
+  let add = 0;
+  let averageNumber = 0;
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("values ", values);
+    console.log("operation ", setOperation);
+    console.log("result ", setResult);
+
+    if (!values) {
+      setResult("Invalid input.")
+    }
+
+    if (!operation) {
+      setResult("Invalid input.")
+    }
+
+    if (operation === "sum") {
+       setResult(+ add)
+    } else if (operation === "average") {
+      averageNumber = add / values.length;
+      setResult(+ averageNumber);
+    }
+  }
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
+      <form onSubmit={handleSubmit}>
+        <input 
+        id="values" 
+        name="values" 
+        type="text" 
+        value={values}
+        onChange={(e) => setValues(e.target.value)} 
+        />
+        <select 
+        id="operation" 
+        name="operation"
+        value={operation}
+        onChange={(e) => setOperation(e.target.value)}
+        >
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
         </select>
         <button type="submit">Calculate</button>
       </form>
