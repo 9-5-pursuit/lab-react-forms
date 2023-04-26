d8d68218662c203dffd94b470cfa979a4fa38d82
lgxxhi <luisgsilva95@gmail.com>
2023-04-26 09:59:29 -0400

    Lab-Semi-Complete

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,75 @@
-import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [values, setValues] = useState("");
+  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState("");
+  const valuesArr = values.split(",").map(Number);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleOperation();
+    handleReset();
+  }
+
+  function handleOperation() {
+    for (let i = 0; i < valuesArr.length; i++) {
+      if (isNaN(valuesArr[i]) || !values || !operation) {
+        return setResult("Invalid Input.");
+      }
+    }
+
+    if (operation === "sum") {
+      setResult(
+        valuesArr.reduce(function (previousValue, currentValue) {
+          return previousValue + currentValue;
+        })
+      );
+    } else if (operation === "average") {
+      const sum = valuesArr.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue;
+      });
+      setResult(sum / valuesArr.length);
+    }
+  }
+
+  function handleReset() {
+    setValues("");
+    setOperation("");
+  }
+
+  function handleResultReset() {
+    setResult("");
+  }
+
+  // function handleMode(array) {
+  //   const obj = {};
+  //   array.forEach((number) => {
+  //     if (!obj[number]) {
+  //       obj[number] = 1;
+  //     } else {
+  //     }
+  //   });
+  // }
+
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input
+          onClick={handleResultReset}
+          id="values"
+          name="values"
+          type="text"
+          value={values}
+          onChange={(e) => setValues(e.target.value)}
+        />
+        <select
+          id="operation"
+          name="operation"
+          value={operation}
+          onChange={(e) => setOperation(e.target.value)}
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -14,8 +77,9 @@ function Form() {
         </select>
         <button type="submit">Calculate</button>
       </form>
+
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </>
   );
