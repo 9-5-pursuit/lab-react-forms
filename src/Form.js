0d34aec87ddd8a72b49453245aaea4e9bf0fb236
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-04-27 00:25:29 -0400

    Completed Lab

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
-  return (
+
+ // useState declarations 
+const [values, setValues] = useState("");
+const [operation, setOperation] = useState("");
+const [valid, setValid] = useState(true);
+const [result, setResult] = useState(0);
+
+// Helper Functions 
+function convertToNumberArray(){
+  return values.split(",").map((number) => parseInt(number));
+}
+
+function handleValid(numberArray){
+  if(numberArray.length === 0 || operation === ""){
+    setValid(false)
+  }
+
+  numberArray.map((number) => {
+    if(isNaN(number) === true){
+      setValid(false)
+    }
+    return null;
+  })
+}
+
+function sum(numberArray){
+  let total = 0;
+  for(let i = 0; i < numberArray.length; i++){
+    total += numberArray[i];
+  }
+  return total;
+}
+
+function average(numberArray){
+  let average = (sum(numberArray) / numberArray.length);
+  return average;
+}
+
+function mode(numberArray){
+  numberArray.sort();
+
+  let maxCount = 0;
+  let currentCount = 0;
+  let value = 0;
+
+  for(let i = 0; i < numberArray.length; i++){
+    if(numberArray[i] === numberArray[i + 1]){
+      currentCount++;
+    }
+    if(currentCount > maxCount){
+      maxCount = currentCount;
+      value = numberArray[i];
+    }
+  }
+  return value;
+}
+
+function resetForm(){
+  setValues("");
+  setOperation("");
+}
+
+
+
+// Function that runs after clicking calculate button
+function handleSubmit(event){
+  event.preventDefault();
+  setValid(true);
+
+  let numberArray = convertToNumberArray();
+
+  handleValid(numberArray);
+
+  if(operation === "sum"){
+    setResult(sum(numberArray));
+  }
+  if(operation === 'average'){
+    setResult(average(numberArray));
+  }
+  if(operation === 'mode'){
+    setResult(mode(numberArray));
+  }
+
+  if(valid === true){
+    resetForm();
+  }
+
+}
+
+
+
+return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input id="values" name="values" value={values} type="text" onChange={(e) => setValues(e.target.value)} />
+        <select id="operation" name="operation" value={operation} onChange={(e) => setOperation(e.target.value)}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -15,7 +106,7 @@ function Form() {
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{valid ? result : 'Invalid input.'}</p>
       </section>
     </>
   );
