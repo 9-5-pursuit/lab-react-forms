0d331c780050171acca02c6853cea2128eb0cd0e
Michael Dwayne Crayton <mdc@Michaels-Air.lan>
2023-04-29 14:02:58 -0400

    added fn and form

--- src/Form.js
+++ src/Form.js
@@ -9,6 +9,88 @@ function Form() {
   const [operation, setOperation] = useState("");
   const [result, setResult] = useState(0);
 
+  /* functions */
+
+  //fn to convert input and seperate with commas then parse into number
+  function convertInputToArray() {
+    return input.split(",").map((number) => parseInt(number));
+  }
+  //fn to makes sure input is number
+  function isValid(inputArray) {
+    if (inputArray.length === 0 || operation === "") {
+      setValid(false);
+    }
+
+    inputArray.map((number) => {
+      if (isNaN(number) === true) {
+        setValid(false);
+      }
+      return null;
+    });
+  }
+  // fn adds numbers in array together
+  function sum(inputArray) {
+    let total = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+      total += inputArray[i];
+    }
+    return total;
+  }
+  //fn finds average of numbers in array
+  function average(inputArray) {
+    let average = sum(inputArray) / inputArray.length;
+    return average;
+  }
+  //fn finds mode of numbers in array
+  function mode(inputArray) {
+    inputArray.sort();
+
+    let totalCount = 0;
+    let currentCount = 0;
+    let most = 0;
+
+    for (let i = 0; i < inputArray.length; i++) {
+      if (inputArray[i] === inputArray[i + 1]) {
+        currentCount++;
+      }
+      if (currentCount > totalCount) {
+        totalCount = currentCount;
+        most = inputArray[i];
+      }
+    }
+    return most;
+  }
+  // resets form fields
+  function resetForm() {
+    setInput("");
+    setOperation("");
+  }
+
+  /* Click event*/
+  function clickSubmit(event) {
+    event.preventDefault();
+
+    setValid(true);
+
+    let inputArray = convertInputToArray();
+
+    isValid(inputArray);
+
+    if (operation === "sum") {
+      setResult(sum(inputArray));
+    }
+    if (operation === "average") {
+      setResult(average(inputArray));
+    }
+    if (operation === "mode") {
+      setResult(mode(inputArray));
+    }
+
+    if (valid === true) {
+      resetForm();
+    }
+  }
+
   return (
     <>
       <form onSubmit={clickSubmit}>
