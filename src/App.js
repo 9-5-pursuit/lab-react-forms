3a477a8445a2807609c317601c4cf0672b3bab84
Mussie <mussierussom@pursuit.org>
2023-05-21 15:28:47 -0400

    React forms lab completed.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
 function App() {
+  const [selectOption, setSelectOption] = useState("");
+
   return (
     <main>
       <p>Enter each number in the array, separated by a ','</p>
-      <Form />
+      <Form
+        selectOption={selectOption}
+        // handleSelectChange={handleSelectChange}
+      />
     </main>
   );
 }
